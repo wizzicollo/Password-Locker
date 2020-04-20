96ebb7190f5ec8beb04367bd0f8abf1d0d6e3cfd
Frozen Feelings <kiprutohcollo@gmail.com>
2020-04-20 15:26:23 +0300

    created user password and be able to generate user random password

--- run.py
+++ run.py
@@ -3,3 +3,30 @@ from password import User, Credential
 import random
 import string
 import getpass
+
+
+
+def create_user(name, user_password):
+    """
+    Parameters
+    ----------
+    name
+    user_password
+    Returns
+    -------
+    """
+    new_user = User(name, user_password)
+    return new_user
+
+
+def generate_password(user):
+    """
+    function to generate random password for user
+    ----------
+    user
+    Returns
+    -------
+    """
+    return user.generate_random_password()
+
+
