2c1a049c08d5b1180310eb85366cb018e6094216
Frozen Feelings <kiprutohcollo@gmail.com>
2020-04-20 14:55:05 +0300

    added classmethods

--- password.py
+++ password.py
@@ -46,3 +46,22 @@ class User:
 
     def test_save_credential(self):
         pass
+
+    @classmethod
+    def find_by_account_username(cls, account_username):
+        for credential in cls.credential_list:
+            if credential.account_username == account_username:
+                return credential
+    @classmethod
+    def credential_exists(cls, account_username):
+        for credential in cls.credential_list:
+            if credential.account_username == account_username:
+                return True
+            return False
+    @classmethod
+    def display_credentials(cls):
+        return cls.credential_list
+
+    @classmethod
+    def display_all_credentials(cls):
+        return cls.credential_list   
