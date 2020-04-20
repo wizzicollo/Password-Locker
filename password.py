b90c97496ed98f4e328585df31d4bb41b1c6eaf2
Frozen Feelings <kiprutohcollo@gmail.com>
2020-04-20 14:40:39 +0300

    added class Credential

--- password.py
+++ password.py
@@ -19,3 +19,30 @@ class User:
         delete_user method deletes saved contact
         """
         User.user_list.remove(self)
+
+
+    class Credential:
+        """
+        Class that generates new instances of credentials
+        """
+        credential_list = []
+
+    def __init__(self, account, account_username, account_password):
+        self.account = account
+        self.account_username = account_username
+        self.account_password = account_password
+
+    def save_credential(self):
+        """
+        save_credential method saves user objects into credential_list
+        """
+        Credential.credential_list.append(self)
+
+    def delete_credential(self):
+        """
+        delete_credential method deletes saved contact
+        """
+        Credential.credential_list.remove(self)
+
+    def test_save_credential(self):
+        pass
